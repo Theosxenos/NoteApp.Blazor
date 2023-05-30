cfb4ffaaaab5bdabe1026b4e214893ec87f5e884
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2023-05-30 15:57:14 +0200

    ASF

---
+++ NoteApp.Blazor/Services/NotebookService.cs
@@ -0,0 +1,24 @@
+﻿namespace NoteApp.Blazor.Services;
+
+public class NotebookService
+{
+	private List<Notebook> Notebooks { get; set; } = new List<Notebook>();
+
+    public Notebook ActiveNotebook { get; set; }
+
+    public NotebookService()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            Notebooks.Add(new Notebook() { NotebookId = i, Name = $"Notebook {i}"});
+        }
+
+        ActiveNotebook = Notebooks[0];
+        Notebooks[0].IsActive = true;
+    }
+
+    public Task<List<Notebook>> GetNotebooksAsync()
+    {
+        return Task.FromResult(Notebooks);
+    }
+}
