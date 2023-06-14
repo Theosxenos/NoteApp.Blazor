1b03d6587aca06cca290d0792b705a42b0f933cc
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2023-06-14 12:26:33 +0200

    Format NotebookService.cs

--- NoteApp.Blazor/Services/NotebookService.cs
+++ NoteApp.Blazor/Services/NotebookService.cs
@@ -3,33 +3,33 @@
 public class NotebookService
 {
 	private List<Notebook> Notebooks { get; set; } = new List<Notebook>();
-    private int lastNotebookId = -1;
+	private int lastNotebookId = -1;
 
-    public Notebook ActiveNotebook { get; set; }
+	public Notebook ActiveNotebook { get; set; }
 
-    public NotebookService()
-    {
-        for(int i = 0; i < 3; i++)
-        {
-            Notebooks.Add(new Notebook() { NotebookId = i, Name = $"Notebook {i}"});
-        }
+	public NotebookService()
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			Notebooks.Add(new Notebook() { NotebookId = i, Name = $"Notebook {i}" });
+		}
 
-        ActiveNotebook = Notebooks[0];
-        Notebooks[0].IsActive = true;
-    }
+		ActiveNotebook = Notebooks[0];
+		Notebooks[0].IsActive = true;
+	}
 
-    public Task<List<Notebook>> GetNotebooksAsync()
-    {
-        return Task.FromResult(Notebooks);
-    }
+	public Task<List<Notebook>> GetNotebooksAsync()
+	{
+		return Task.FromResult(Notebooks);
+	}
 
-    public void AddNewNotebook(string name)
-    {
-        if(lastNotebookId  == -1)
-        {
-            lastNotebookId = Notebooks.Last().NotebookId;
-        }
-        Notebook newNotebook = new Notebook() { Name = name, NotebookId= ++lastNotebookId};
-        Notebooks.Add(newNotebook);
-    }
+	public void AddNewNotebook(string name)
+	{
+		if (lastNotebookId == -1)
+		{
+			lastNotebookId = Notebooks.Last().NotebookId;
+		}
+		Notebook newNotebook = new Notebook() { Name = name, NotebookId = ++lastNotebookId };
+		Notebooks.Add(newNotebook);
+	}
 }
